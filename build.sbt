86e5a66a998cfcb431ac8c470e80f43d0b726abf
Steve King <stevejohnking@gmail.com>
2017-05-17 14:48:59 -0700

    Add debug logging to Reshard Table compute function

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "cornucopia"
 organization := "com.github.kliewkliew"
 
 //version := "1.1.2"
-version := "0.23-SNAPSHOT"
+version := "0.24-SNAPSHOT"
 
 scalaVersion := "2.11.8"
 
--- src/main/scala/com/github/kliewkliew/cornucopia/redis/ReshardTable.scala
+++ src/main/scala/com/github/kliewkliew/cornucopia/redis/ReshardTable.scala
@@ -1,5 +1,6 @@
 package com.github.kliewkliew.cornucopia.redis
 
+import org.slf4j.LoggerFactory
 import com.lambdaworks.redis.cluster.models.partitions.RedisClusterNode
 
 object ReshardTable {
@@ -8,10 +9,14 @@ object ReshardTable {
   type Slot = Int
   type ReshardTable = scala.collection.immutable.Map[NodeId, List[Slot]]
 
+  case class LogicalNode(node: RedisClusterNode, slots: List[Int])
+
+  final val ExpectedTotalNumberSlots: Int = 16384
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
   def computeReshardTable(sourceNodes: List[RedisClusterNode]): ReshardTable = {
     import scala.collection.JavaConverters._
-
-    case class LogicalNode(node: RedisClusterNode, slots: List[Int])
 
     val logicalNodes = sourceNodes.map { n =>
       val slots = n.getSlots.asScala.toList.map(_.toInt)
@@ -20,9 +25,19 @@ object ReshardTable {
 
     val sortedSources = logicalNodes.sorted(Ordering.by((_: LogicalNode).slots.size).reverse)
 
+    printSortedSources(sortedSources)
+
     val totalSourceSlots = sortedSources.foldLeft(0)((sum, n) => sum + n.slots.size)
 
+    logger.debug(s"Reshard table total sources: $totalSourceSlots")
+
+    if (totalSourceSlots != ExpectedTotalNumberSlots) {
+      logger.error(s"Reshard table total source slots is $totalSourceSlots, but is not equal to expected number $ExpectedTotalNumberSlots")
+    }
+
     val numSlots = totalSourceSlots / (logicalNodes.size + 1) // total number of slots to move to target
+
+    logger.debug(s"Reshard table total number of slots to move to target: $numSlots")
 
     def computeNumSlots(i: Int, source: LogicalNode): Int = {
       if (i == 0) Math.ceil((numSlots.toFloat / totalSourceSlots) * source.slots.size).toInt
@@ -36,9 +51,16 @@ object ReshardTable {
       val n = computeNumSlots(i, source)
       val slots = sortedSlots.take(n)
       val nodeId = source.node.getNodeId
+      logger.debug(s"Reshard table adding $n slots from $nodeId to move to target")
       tbl + (nodeId -> slots)
     }
 
     table
   }
+
+  private def printSortedSources(sources: List[LogicalNode]): Unit = {
+    logger.debug(s"Reshard table sorted source slots:")
+    sources.foreach(n => logger.debug(s"${n.node.getNodeId} has ${n.slots.size} slots: ${n.slots}"))
+  }
+
 }
