f487c2c31f578903941af5002508b7e4e1110241
Steve King <stevejohnking@gmail.com>
2017-07-28 11:19:00 -0700

    Get connections to all nodes, not just masters

--- src/main/scala/com/adendamedia/cornucopia/redis/ClusterOperations.scala
+++ src/main/scala/com/adendamedia/cornucopia/redis/ClusterOperations.scala
@@ -286,16 +286,16 @@ object ClusterOperationsImpl extends ClusterOperations {
 
     implicit val saladAPI = newSaladAPI
 
-    val liveMasters: Future[List[RedisClusterNode]] = saladAPI.masterNodes.map { masters =>
-      masters.toList.filter(_.isConnected)
+    val liveNodes: Future[List[RedisClusterNode]] = saladAPI.clusterNodes.map { nodes =>
+      nodes.toList.filter(_.isConnected)
     }
 
     val connections: Future[List[(RedisClusterNode, Future[Connection.Salad])]] = for {
-      masters <- liveMasters
+      nodes <- liveNodes
     } yield {
       for {
-        master <- masters
-      } yield (master, getConnection(master.getNodeId))
+        node <- nodes
+      } yield (node, getConnection(node.getNodeId))
     }
 
     val result: Future[List[((NodeId, RedisUriString), Connection.Salad)]] = connections.flatMap { conns =>
